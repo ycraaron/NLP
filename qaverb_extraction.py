079a687b99c78ef180f640eb0688f44c57740766
Aaron Yang <ycr_aaron@foxmail.com>
2017-11-04 00:28:07 +0800

    qa verb pos extraction

---
+++ qaverb_extraction.py
@@ -0,0 +1,78 @@
+from pycorenlp import StanfordCoreNLP
+from mysql_utils.db_manager import DBConn
+
+nlp = StanfordCoreNLP('http://192.168.0.100:9000')
+
+
+def stanford_tree(line, annotators='pos'):
+    output = nlp.annotate(line, properties={
+        'annotators': annotators,
+        'outputFormat': 'json'
+    })
+    try:
+        return output
+    except IndexError:
+        pass
+
+
+def load_data():
+    db_conn = DBConn()
+    ls_result = db_conn.fetch_data('SELECT user_input, lib_response FROM whatsapp_record_slot')
+    ls_user_intput = []
+    ls_lib_respon = []
+    for record in ls_result:
+        ls_user_intput.append(record['user_input'])
+        ls_lib_respon.append(record['lib_response'])
+
+    return ls_user_intput, ls_lib_respon
+
+
+def deal_qa(msg):
+    result = stanford_tree(msg)
+    print(result)
+    quit()
+
+
+def entry():
+    ls_msg, ls_respon = load_data()
+
+    print(len(ls_msg), len(ls_respon))
+
+    i = 0  # current msg start
+    while True:
+        msg = ''
+        respon = ''
+        next_msg_start = 0
+        end_tag = 0
+        if ls_msg[i] != '':
+            for j in range(i+1, len(ls_respon)):
+                if ls_respon[j] != '':
+                    print('respond id', j)
+                    respon = ls_respon[j]
+                    if j+1 == len(ls_respon):
+                        end_tag = 1
+                    for k in range(j+1, len(ls_respon)):
+                        if ls_msg[k] != '':
+                            if next_msg_start == 0:
+                                next_msg_start = k
+                            break
+                        else:
+                            if end_tag == 1:
+                                break
+                            respon += ls_respon[k]
+                    break
+                else:
+                    continue
+            if next_msg_start == 0: # end of conversation
+                msg = ls_msg[i]
+            for m in range(i, next_msg_start):
+                msg += ls_msg[m]
+        i = next_msg_start  # update next msg start
+        print('msg', msg)
+        print('response', respon)
+        print('new msg id', next_msg_start)
+        if end_tag == 1:
+            break
+        ls_verb_msg = deal_qa(msg)
+        deal_qa(respon)
+entry()
